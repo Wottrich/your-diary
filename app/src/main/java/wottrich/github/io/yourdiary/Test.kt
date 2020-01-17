86b19e12c6eb3a03294a1d9b5f5d6d2c9a46ccb8
wottrich <lucas.wottrich@operacao.rcadigital.com.br>
2020-01-17 09:12:38 -0300

    Firebase

--- app/src/main/java/wottrich/github/io/yourdiary/Test.kt
+++ app/src/main/java/wottrich/github/io/yourdiary/Test.kt
@@ -1,8 +1,16 @@
 package wottrich.github.io.yourdiary
 
+import wottrich.github.io.yourdiary.extensions._locale
+import wottrich.github.io.yourdiary.extensions.convertToDouble
+import wottrich.github.io.yourdiary.extensions.format
+
 
 fun main() {
 
-    val json = "\n\n  {\n    \"resposta\": {\n      \"success\": true,\n      \"message\": \"OK\",\n      \"data\": {\n        \"carrocel\": [\n          {\n            \"carTitulo\": \"teste do carrocel \",\n            \"carLinkLoja\": \"http://loja.com.br/teste\",\n            \"carLinkImagem\": \"http://imagem.com.br/teste.jpg\"\n          }\n        ],\n        \"promoção\": [\n          {\n            \"proTitulo\": \"SMART TV 32\\\" LED PHG5102 PHILIPS\",\n            \"proLinkLoja\": \" http://loja.com.br/teste \",\n            \"proLinkImagem\": \" http://imagem.com.br/teste.jpg \"\n          }\n        ]\n      }\n    }\n  }"
+    val a = "9.03"
+    val b = convertToDouble(a, _locale)
+    val c = b.format()
+
+    println(c)
 
 }
